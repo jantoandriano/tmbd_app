29129d7b6dc63fa56e36e9b4931b37db24b6e287
jantoandriano <janto.motulo@hyperscal.com>
2026-08-14 11:25:54 +0700

    feat: add get_it/injectable DI scaffolding

---
+++ lib/core/di/injection.dart
@@ -0,0 +1,8 @@
+import 'package:cinetrack/core/di/injection.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final GetIt getIt = GetIt.instance;
+
+@InjectableInit(preferRelativeImports: true)
+Future<void> configureDependencies() async => getIt.init();
---
+++ lib/core/di/register_module.dart
@@ -0,0 +1,12 @@
+import 'package:cinetrack/core/constants/api_constants.dart';
+import 'package:cinetrack/core/network/dio_client.dart';
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+/// Registers third-party/manually-constructed instances that injectable
+/// can't build via its own `@injectable` annotations.
+@module
+abstract class RegisterModule {
+  @lazySingleton
+  Dio get dio => DioClient.create(baseUrl: ApiConstants.tmdbBaseUrl);
+}
